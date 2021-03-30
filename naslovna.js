899945fdcdd850f4a6185ede7870ed1c36e9ccbf
Cr4zyCRO <bruno.galic02@gmail.com>
2021-03-30 21:08:29 +0200

    naboj napon i struja

--- naslovna.js
+++ naslovna.js
@@ -1,1 +1,6 @@
 M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
