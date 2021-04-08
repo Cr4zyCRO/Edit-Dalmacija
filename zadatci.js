1c951453bb7b785c6893cc26847b92d41e74ff0a
Andelo-p <andelopodrug6@gmail.com>
2021-04-08 11:21:04 +0200

    add pic for zad & more zad

--- zadatci.js
+++ zadatci.js
@@ -1,23 +1,23 @@
-function myFunction(){
-    let a = Number(prompt("Unesite vrijednost napona izvora U"))
-    let b = Number(prompt("Unesite vrijednost otpora R1"))
-    let c = Number(prompt("Unesite vrijednost otpora R2"))
+function zadatak1(){
+    let naponizvora= Number(prompt("Unesite vrijednost napona izvora U"))
+    let otpor1 = Number(prompt("Unesite vrijednost otpora R1"))
+    let otpor2 = Number(prompt("Unesite vrijednost otpora R2"))
 
-            iotpor = b + c
-            istruja = a / iotpor
-            inapon1 = istruja * b
-            inapon2 = istruja * c
-            isnaga1 = istruja * iu1
-            isnaga2 = istruja * iu2
+            ukupniotpor = otpor1 + otpor2
+            ukupnastruja = naponizvora / ukupniotpor
+            padnapona1 = ukupnastruja * otpor1
+            padnapona2 = ukupnastruja * otpor2
+            snaga1 = padnapona1 * ukupnastruja
+            snaga2 = padnapona2 * ukupnastruja
 
-        alert("Struja koju daje izvor je " + istruja +  "A") 
-        alert("Pad napona na otporniku R1 je " + inapon1 + "V")
-        alert("pad napona na otporniku R2 je " + inapon2+ "V")
-        alert("Snaga otpornika R1 je "+ isnaga1 + "W")
-        alert("Snaga otpornika R2 je "+ isnaga2 + "W")
+        alert("Struja koju daje izvor je " + ukupnastruja + "A")
+        alert("Pad napona na otporniku R1 je " + padnapona1 + "V")
+        alert("Pad napona na otporniku R2 je " + padnapona2 + "V")
+        alert("Snaga na otporu R1 je " + snaga1 + "W")
+        alert("Snaga na otporu R2 je " + snaga2 + "W")
 }
 
-function myFunction1(){
+function zadatak2(){
     let a = Number(prompt("Unesite vrijednost otpornika R3"))
     let b = Number(prompt("Unesite vrijednost struje I3"))
     let c = Number(prompt("Unesite vrijednost otpora R2"))
@@ -34,19 +34,34 @@ function myFunction1(){
         alert("Vrijednost otpora R1 je " + ir1 + "Ohma") 
         alert("Napon izvora je " + iui + "V")
 }
-function myFunction2(){
-    let a = Number(prompt("Unesite vrijednost otpornika R1"))
-    let b = Number(prompt("Unesite vrijednost otpornika R2"))
-    let c = Number(prompt("Unesite vrijednost otpornika R3"))
-    let d = Number(prompt("Unesite vrijednost otpornika R4"))
-    let e = Number(prompt("Unesite vrijednost napona izvora"))
+function zadatak3(){
+    let naponizvorar = Number(prompt("Unesite vrijednost napon izvora"))
+    let otporr1 = Number(prompt("Unesite vrijednost otpora R1"))
+    let otporr2 = Number(prompt("Unesite vrijednost otpora R2"))
+    let otporr3 = Number(prompt("Unesite vrijednost otpora R3"))
+    let otporr4 = Number(prompt("Unesite vrijednost otpora R4"))
 
-            iotpor12 = ( 1 / a ) + ( 1 / b )
-            iotpor123 = ( 1 / c ) + ( 1 / iotpor12)
-            iotporuk = iotpor123 + d
-            istruja1 = e / iotporuk
-            inaponp = istruja1 * iotpor123
-            istruaj2 = inaponp / iotpor12
-            
-        alert(inaponab)
+            otporr12 = (1 / otporr1) + (1 /otporr2)
+            otporr123 = (1 / otporr12) + (1 / otporr3)   
+            otporr1234 = otporr123 + otporr4
+            ukupnastrujar = naponizvorar / otporr1234
+            naponapabr = ukupnastrujar * otporr123
+            ampermetarr = naponapabr / otporr12
+
+        alert("Struja koju pokazuje ampermetar je " + ampermetarr + "A")
 }
+function zadatak4(){
+    let otport = Number(prompt("Unesite iznos otpora R"))
+    let induktivitett = Number(prompt("Unesite iznos zavojnice L(u H)"))
+    let kapacitett = Number(prompt("Unesite iznos kondezatora C (u F)"))
+    let naponizovrat = Number(prompt("Unesite iznos napona V"))
+    let frekvencijat = Number(prompt("Unesite frekvenciju f"))
+
+            impinduktivitett = 2 * 3.14159265359 * frekvencijat * induktivitett
+            impkapacitett = 1 / (2 * 3.1415925359 * frekvencijat * kapacitett)
+            impedancijat = otport + impinduktivitett + impkapacitett
+            strujat = naponizovrat / impedancijat
+        
+        alert("Struja koju daje izvor je " + strujat + "A")
+}
+
